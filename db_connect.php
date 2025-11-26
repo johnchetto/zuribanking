1efa0ade144c86ece24a9aea0f8b533d2d3de0a8
John Macharia <johnchetto@12gmail.com>
2025-11-26 22:43:48 +0300

    Update db_connect.php for local and live database connection

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,20 @@
 <?php
-$servername = "sql100.infinityfree.com";
-$username = "if0_40527894";
-$password = "j1QRqytZ4nhUm";
-$dbname = "if0_40527894_XXX"; // updated database name
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    // Local XAMPP
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "zuri_db"; // your local DB
+} else {
+    // InfinityFree live server
+    $servername = "sql100.infinityfree.com";
+    $username = "if0_40527894";
+    $password = "j1QRqytZ4nhUm";
+    $dbname = "if0_40527894_XXX"; // InfinityFree DB
+}
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-/* creation of connection */
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
