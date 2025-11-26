ceba52647f7578844073bccb252d21362711ad86
John Macharia <johnchetto@12gmail.com>
2025-11-26 21:03:47 +0300

    Your descriptive commit message here

--- db_connect.php
+++ db_connect.php
@@ -6,7 +6,7 @@ $dbname = "if0_40527894_XXX"; // updated database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+/* creation of connection */
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
