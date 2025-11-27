d208a6d93fd9fa7a74c37cd5eff10ca39d994b57
John Macharia <johnchetto@12gmail.com>
2025-11-27 10:17:51 +0300

    Update Transaction_customer.php

--- Transaction_customer.php
+++ Transaction_customer.php
@@ -109,7 +109,7 @@ $transactions = $result->fetch_all(MYSQLI_ASSOC);
             <li><a href="dashboard_customer.php">Dashboard</a></li>
             <li><a href="balance_customer.php">Balance</a></li>
             <li><a href="transfer_customer.php">Transfer</a></li>
-            <li><a href="transaction_customer.php">Transactions</a></li>
+            <li><a href="Transaction_customer.php">Transactions</a></li>
             <li><a href="profile_customer.php">Profile</a></li>
             <hr>
             <li><a href="customer_support.php">Need Support</a></li>
